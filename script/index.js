61bb10a2e081a3b046e591a782a8aa8e0918712e
3yh4nh <34964908+3yh4nh@users.noreply.github.com>
2022-11-10 20:19:47 +0100

    Update forecast section

--- script/index.js
+++ script/index.js
@@ -108,6 +108,34 @@ function changeToCelcius(event) {
   document.querySelector("#degree-F").style.color = "#616074";
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">${day}
+          <br />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            class="bi small-icon"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973z"
+            />
+          </svg>
+          <br />
+          13<strong>/23°</strong>
+        </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 changeTime();
 
 let enterButton = document.querySelector(".city-form");
@@ -128,3 +156,4 @@ let units = "metric";
 let temperatureC = null;
 let iconFileName = null;
 search("Paris");
+displayForecast();
