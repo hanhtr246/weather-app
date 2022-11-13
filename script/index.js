ee96d21dfaa8e5f5b2e43aa7a599dbcc8763b707
3yh4nh <34964908+3yh4nh@users.noreply.github.com>
2022-11-13 17:33:12 +0100

    Update appearance

--- script/index.js
+++ script/index.js
@@ -155,7 +155,9 @@ function displayForecast(response) {
         forecastDay.weather[0].main,
         forecastDay.weather[0].icon
       );
-      forecastHTML += `<div class="col chiacot">${formatDay(forecastDay.dt)}
+      forecastHTML += `<div class="col forecast-cols">${formatDay(
+        forecastDay.dt
+      )}
           <div>
           <svg class="small-icon"><image class="bi" width="30px" height="30px" href="images/${iconForecast}.svg"/></svg>
           </div>
